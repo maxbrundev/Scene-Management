aeaa32b0d5f5fcd2ddaa9ef3428cf09600cd3619
Max Brun <maxbrundev@gmail.com>
2023-07-24 22:29:42 +0200

    Adding Scene Entry Point Project Settings

---
+++ Editor/ProjectSettings/Game/SceneEntryPoint/SceneEntryProjectSetting.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+static class SceneEntryProjectSetting
+{
+	static SceneEntryProjectSetting()
+	{
+		var entryPoint = SceneEntryProjectSettingData.GetSettings();
+
+		if (entryPoint != null)
+		{
+			entryPoint.UpdateEditorOnPlay();
+		}
+	}
+
+	[SettingsProvider]
+	public static SettingsProvider CreateMyCustomSettingsProvider()
+	{
+		var provider = new SettingsProvider("Project/SceneEntryProjectSetting", SettingsScope.Project)
+		{
+			label = EditorConstants.SCENE_ENTRY_LABEL_NAME,
+
+			guiHandler = (searchContext) =>
+			{
+				var settings = SceneEntryProjectSettingData.GetSerializedSettings();
+				EditorGUILayout.PropertyField(settings.FindProperty("data"), new GUIContent("Scene Entry Point"));
+				settings.ApplyModifiedPropertiesWithoutUndo();
+			},
+
+			keywords = new HashSet<string>(new[] { "Scene Entry Point" })
+		};
+
+		return provider;
+	}
+}
---
+++ Editor/ProjectSettings/Game/SceneEntryPoint/SceneEntryProjectSettingData.cs
@@ -0,0 +1,38 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SceneEntryProjectSettingData", menuName = "Editor/Data/SceneEntry")]
+class SceneEntryProjectSettingData : ScriptableObject
+{
+	[SerializeField] public SceneData data;
+
+	void OnValidate()
+	{
+		UpdateEditorOnPlay();
+	}
+
+	public void UpdateEditorOnPlay()
+	{
+		if (data == null || data.Scene.BuildIndex > EditorBuildSettings.scenes.Length - 1)
+		{
+			EditorSceneManager.playModeStartScene = null;
+			return;
+		}
+
+		EditorSceneManager.playModeStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(EditorBuildSettings.scenes[data.Scene.BuildIndex].path);
+	}
+
+	internal static SceneEntryProjectSettingData GetSettings()
+	{
+		var settings = AssetDatabase.LoadAssetAtPath<SceneEntryProjectSettingData>(EditorConstants.SCENE_ENTRY_SETTING_ASSET_PATH);
+
+		return settings;
+	}
+
+	internal static SerializedObject GetSerializedSettings()
+	{
+		return new SerializedObject(GetSettings());
+	}
+}
