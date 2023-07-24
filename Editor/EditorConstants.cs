ddc031f2cdb1b7c0b9ae698b14638f80a867cc5c
Max Brun <maxbrundev@gmail.com>
2023-07-24 23:39:05 +0200

    Adding missing EditorConstants struct

---
+++ Editor/EditorConstants.cs
@@ -0,0 +1,4 @@
+public struct EditorConstants
+{
+    public const string SCENE_ENTRY_SETTING_ASSET_PATH    = "Assets/Scripts/Editor/ProjectSettings/Game/SceneEntryPoint/SceneEntryProjectSettingData.asset";
+}
